334a53310e58415fd29246bec015ff1b3c9c1801
Precious Oyebode <preckidtech@gmail.com>
2025-09-29 23:03:19 +0100

    added predict page

---
+++ bank-churn-frontend/src/pages/Predict.jsx
@@ -0,0 +1,11 @@
+import PredictionForm from "../components/PredictionForm";
+
+function Predict() {
+  return (
+    <section className="py-12 px-6 bg-gray-50 min-h-screen">
+      <PredictionForm />
+    </section>
+  );
+}
+
+export default Predict;
