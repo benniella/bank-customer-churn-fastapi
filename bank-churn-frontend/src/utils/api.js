6b6601f03fd953060d05ed7b560e006a752395cc
Precious Oyebode <preckidtech@gmail.com>
2025-10-01 23:52:53 +0100

    new update on github action

--- bank-churn-frontend/src/utils/api.js
+++ bank-churn-frontend/src/utils/api.js
@@ -1,19 +1,19 @@
-const API_URL = "https://bank-customer-churn-fastapi.onrender.com";
+// const API_URL = "https://bank-customer-churn-fastapi.onrender.com";
 
-export async function predictChurn(customerData) {
-  try {
-    const res = await fetch(`${API_URL}/predict`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(customerData),
-    });
+// export async function predictChurn(customerData) {
+//   try {
+//     const res = await fetch(`${API_URL}/predict`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(customerData),
+//     });
 
-    if (!res.ok) throw new Error("Failed to fetch prediction");
-    return await res.json();
-  } catch (err) {
-    console.error("API Error:", err);
-    return { prediction: "Error", churn_probability: 0 };
-  }
-}
+//     if (!res.ok) throw new Error("Failed to fetch prediction");
+//     return await res.json();
+//   } catch (err) {
+//     console.error("API Error:", err);
+//     return { prediction: "Error", churn_probability: 0 };
+//   }
+// }
