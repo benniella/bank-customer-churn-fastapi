6dec1322c39d6d77a49eb9ca225baa038f34b618
Oyebode Precious Isaac <preckidtech@gmail.com>
2025-10-04 05:57:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit  
+
+# Start FastAPI with uvicorn
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
