496e8170f6963ce278caef4107f5288bd3c434a8
Precious Oyebode <preckidtech@gmail.com>
2025-10-05 08:36:53 +0100

    update about page

--- frontend/bank-churn-frontend/src/pages/About.jsx
+++ frontend/bank-churn-frontend/src/pages/About.jsx
@@ -4,13 +4,25 @@ export default function About() {
       <div className="container mx-auto max-w-3xl">
         {/* Title */}
         <h2 className="text-4xl font-bold text-gold-400 mb-6">
-          About This Project
+          About the Bank Churn Prediction Model
         </h2>
 
         {/* Short Description */}
         <p className="text-lg text-black-300 leading-relaxed">
-          BankChurn AI empowers banks with predictive insights, helping them  
-          understand customer behavior and reduce churn with smarter decisions.
+          The Bank Customer Churn Prediction Model is a machine learning system
+          designed to estimate the likelihood that a customer will leave (churn)
+          a bank based on their profile and account behavior. <br /> <br /> Using real-world
+          financial data, the model analyzes key customer attributes such as
+          credit score, age, account balance, number of products, card
+          ownership, activity status, and location to generate a churn
+          probability score between 0 and 1. <br /> <br /> A higher score (closer to 1) 
+          indicates that the customer is more likely to churn.  <br />A lower score
+          (closer to 0) suggests that the customer is likely to remain with the
+          bank. <br /> <br />The model was developed, trained, and deployed using Python,
+          FastAPI, and machine learning algorithms to simulate a real-world
+          banking risk analytics solution. It serves as an intelligent
+          decision-support tool for banks aiming to improve customer retention
+          strategies and reduce churn-related losses.
         </p>
       </div>
     </section>
